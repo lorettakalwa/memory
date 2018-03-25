7bacebae69a6b8493772c83a73fb50d2bfba851d
Loretta Kalwa <lorcia92@gmail.com>
2018-03-26 01:40:26 +0200

    Final styles and documentation

---
+++ README.md
@@ -0,0 +1,9 @@
+# Memory Game Project
+## Table of Contents
+* [Description](#description)
+* [Credits](#credits)
+## Description
+Simple Memory game.
+
+## Credits
+Icons made by [Freepik](https://www.flaticon.com/authors/freepik), [Zlatko Najdenovski](https://www.flaticon.com/authors/zlatko-najdenovski) and [Good Ware](https://www.flaticon.com/authors/good-ware) from [www.flaticon.com](https://www.flaticon.com).
--- js/app.js
+++ js/app.js
@@ -7,10 +7,18 @@ let n = 0;
 const game = document.getElementById("game");
 const success = document.getElementById("success");
 const button = document.getElementById("button");
+const timeGame = document.getElementById("timeGame");
+const rating = document.getElementById("rating");
+const moves = document.getElementById("moves");
+const timeSuccess = document.getElementById("timeSuccess");
+const ratingSuccess = document.getElementById("ratingSuccess");
+const movesSuccess = document.getElementById("movesSuccess");
+const bestScore = document.getElementById("bestScore");
 const symbols = ["a","a","b","b","c","c","d","d","e","e","f","f","g","g","h","h"];
 const cardsN = symbols.length;
 let cardsGuessed = Array(cardsN).fill(0);
 let symbolsShuffled = symbols;
+let timer = "";
 
 //Sum
 function sum(a,b) {
@@ -32,6 +40,9 @@ function randomOrder(array) {
 //Initializing game
 function initialize(){
   symbolsShuffled = randomOrder(symbols);
+  timeGame.innerHTML = "0:0";
+  moves.innerHTML = 0;
+  rating.innerHTML = "★★★";
   for (i = 0; i < cardsN; i++){
     game.children[i].innerHTML = "<img src=\"svg\\" + symbolsShuffled[i] + ".png\">";
   };
@@ -41,7 +52,7 @@ function initialize(){
       game.children[i].classList.add("flipped");
       game.children[i].classList.remove("guessed");
     };
-    toggleClass(success,"success");
+    toggleClass(success,"noSuccess");
     cardsGuessed = Array(cardsN).fill(0);
     clicks = 0;
     card = -1;
@@ -59,46 +70,72 @@ function toggleClass(div,className){
 //Card index
 function childIndex(div){
   return Array.prototype.indexOf.call(div.parentNode.children, div);
-}
+};
 
 //Checking if there is a match
 function check(clicked){
   //Exit function if something else than a card was clicked
   if ( clicked.id === "game") {
     return;
-  }
+  };
   //Exit function if a guessed card or already flipped card was clicked
   card = childIndex(clicked);
   if ( cardsGuessed[card] == 1 || card1 == card ) {
     return;
-  }
+  };
+  //Set timer, rating and attemps counter on
   if ( clicks == 0 ){
     let start = new Date().getTime();
-    let timer = setInterval(function() {
+    timer = setInterval(function() {
     let now = new Date().getTime();
     let timePassed = now - start;
     let minutes = Math.floor((timePassed % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timePassed % (1000 * 60)) / 1000);
-    document.getElementById("timeGame").innerHTML = minutes + ":" + seconds;
+    timeGame.innerHTML = minutes + ":" + seconds;
+    moves.innerHTML = Math.floor(clicks / 2);
+    if ( clicks > 30 ) {
+      rating.innerHTML = "★★☆";
+    } else if ( clicks > 50 ){
+      rating.innerHTML = "★☆☆";
+    } else if ( clicks < 31 ) {
+      rating.innerHTML = "★★★";
+    };
   }, 1000);
-  }
+};
+  //Show the icon
   clicks += 1;
   toggleClass(clicked,"flipped");
+  //Keep the icon shown if this is the only one visible
   if ( clicks % 2 === 1){
     card1 = card;
     return;
-  } else {
+  }
+  //If two icons are visible either mark the cards as guessed or flip them
+  else {
     card2 = card;
+    //Check if it's a guessed pair
     if (symbolsShuffled[card1] == symbolsShuffled[card2]) {
       toggleClass(game.children[card1],"guessed");
       toggleClass(clicked,"guessed");
       cardsGuessed[card1] = 1;
       cardsGuessed[card2] = 1;
+      //Check if all cards are guessed
       if ( cardsGuessed.reduce(sum,0) === cardsN ) {
-        toggleClass(success,"success");
         clearInterval(timer);
+        timeSuccess.innerHTML = "Time: " + timeGame.innerHTML;
+        ratingSuccess.innerHTML = "Rating: " + rating.innerHTML;
+        movesSuccess.innerHTML = "Moves: " + Math.floor(clicks / 2);
+        toggleClass(success,"noSuccess");
+        //Using local storage to save and get the best result
+        let first = localStorage.getItem("first");
+        if ( Math.floor(clicks / 2) < first ) {
+          localStorage.setItem("first", Math.floor(clicks / 2));
+        };
+        bestScore.innerHTML = "Best score: " + localStorage.getItem("first");
       };
-    } else {
+    }
+    //Flipping cards in case of not guessing the pair
+    else {
       setTimeout(function() {toggleClass(clicked,"flipped");}, 1000);
       let firstCard = game.children[card1];
       //Error: setTimeout(function() {toggleClass(game.children[card1],"flipped");},1000);
